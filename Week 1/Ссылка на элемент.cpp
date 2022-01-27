090a92daecb399cbd93a836c3b428147c3e01fab
Semen Semenov <semenov.ss@phystech.edu>
2022-01-27 23:11:50 +0500

    Create Ссылка на элемент.cpp

---
+++ Week 1/Ссылка на элемент.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <utility>
+#include <map>
+using namespace std;
+template <typename Key, typename Value> Value& GetRefStrict(const map<Key, Value>& m, Key key);
+
+template <typename Key, typename Value>
+Value& GetRefStrict(map<Key, Value>& m, Key key) {
+    if (m.find(key) == m.end()) {
+        throw runtime_error("");
+    }
+    Value& link = m[key];
+    return link;
+}
+
+int main()
+{
+    map<int, string> m = { {0, "value"} };
+    string& item = GetRefStrict(m, 0);
+    item = "newvalue";
+    cout << m[0] << endl; // выведет newvalue
+    return 0;
+}
