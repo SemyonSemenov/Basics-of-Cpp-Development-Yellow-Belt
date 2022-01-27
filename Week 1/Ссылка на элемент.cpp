b3b58e0331d2c2d9b9a7b83cb79812ea1e51cc2b
Semyon Semenov <62755701+SemyonSemenov@users.noreply.github.com>
2022-01-27 23:13:39 +0500

    Update Ссылка на элемент.cpp

--- Week 1/Ссылка на элемент.cpp
+++ Week 1/Ссылка на элемент.cpp
@@ -20,6 +20,6 @@ int main()
     map<int, string> m = { {0, "value"} };
     string& item = GetRefStrict(m, 0);
     item = "newvalue";
-    cout << m[0] << endl; // выведет newvalue
+    cout << m[0] << endl; // output newvalue
     return 0;
 }
